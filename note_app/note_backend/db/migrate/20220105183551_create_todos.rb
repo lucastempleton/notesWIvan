7c0aba50b48d9122984c4502e4e75dac9c7aea59
Lucas Templeton <templetl99@gmail.com>
2022-01-05 13:34:21 -0600

    backend added

--- note_app/note_backend/db/migrate/20220105183551_create_todos.rb
+++ note_app/note_backend/db/migrate/20220105183551_create_todos.rb
@@ -1,4 +1,9 @@
 class CreateTodos < ActiveRecord::Migration[6.1]
   def change
+    create_table :todos do |t|
+      t.string :name
+      t.string :task
+      t.references :category
+    end
   end
 end
--- note_app/note_backend/db/migrate/20220105183558_create_categories.rb
+++ note_app/note_backend/db/migrate/20220105183558_create_categories.rb
@@ -1,4 +1,9 @@
 class CreateCategories < ActiveRecord::Migration[6.1]
   def change
+    create_table :categorys do |t|
+      t.string :financial
+      t.string :housework
+      t.string :schoolwork
+      t.string :business
   end
 end
